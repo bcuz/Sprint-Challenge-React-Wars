4d5d19816f45e3d3109fc8dc235c9d048c9e121a
adam <bcuz@users.noreply.github.com>
2019-03-09 14:12:48 -0500

    conditionally add bootstrap class based on index

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Character = props => {
-  return <li className='list-group-item'>{props.person.name}</li>
+  return <li className={`${(props.index % 2 === 0) ? 'list-group-item-info' : ''} list-group-item`}>{props.person.name}</li>
 };
 
 export default Character;
